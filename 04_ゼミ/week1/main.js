c04ad5fa8075874e65e18c8a9bb6f15df153d9a1
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-01-14 21:15:07 +0900

    [□と○]を学習しました。

--- 04_ゼミ/week1/main.js
+++ 04_ゼミ/week1/main.js
@@ -1,11 +1,5 @@
-const display = document.getElementById("display")
-const plusButton = document.getElementById("plus-button")
+const figure = document.getElementById("figure")
 
-let count=0
-
-// ボタンをクリックしたときの処理を登録
-plusButton.onclick = function() {
-    // ここにクリック後の処理を書く
-    count+=1
-    display.textContent=count
+figure.onclick = function() {
+    figure.classList.toggle("rounded")
 }
