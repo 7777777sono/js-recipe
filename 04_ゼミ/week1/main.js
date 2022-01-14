bd073d1cfbcbc5f50388c4e0e9396721421faae9
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-01-14 20:53:25 +0900

    [カウンター]を学習しました。

--- 04_ゼミ/week1/main.js
+++ 04_ゼミ/week1/main.js
@@ -1,18 +1,11 @@
-const genkiFunction = function(number){
-    for(let i=1;i<=number;i++){
-        if(i%3==0&&i%5==0){
-            console.log("FizzBuzz")
-        }
-        else if(i%3==0){
-            console.log("Fizz")
-        }
-        else if(i%5==0){
-            console.log("Buzz")
-        }
-        else{
-            console.log(i)
-        }
-    }
-}
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
 
-genkiFunction(100)
+let count=0
+
+// ボタンをクリックしたときの処理を登録
+plusButton.onclick = function() {
+    // ここにクリック後の処理を書く
+    count+=1
+    display.textContent=count
+}
