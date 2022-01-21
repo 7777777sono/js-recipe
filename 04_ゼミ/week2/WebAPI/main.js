8edec315ca740d795783cdef66952ae4e411f92a
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-01-21 21:33:36 +0900

    [DogAPIで非同期処理]でワンちゃんきりかえボタンを作成しました。

--- 04_ゼミ/week2/WebAPI/main.js
+++ 04_ゼミ/week2/WebAPI/main.js
@@ -1,13 +1,24 @@
 const imageElement = document.getElementById("dog-image")
+const Button = document.getElementById("update")
 
-// 指定したサーバーにデータを取りに行く
-// fetch()を使うときは、ひとつめのthen()の中身は必ず同じで、ふたつめのthen()でdataを使ってなにか処理をする
-fetch("https://dog.ceo/api/breeds/image/random")  // まず fetch でデータの取得をしにいき、
-    .then((res) => {
-        // fetch がおわったら１つめの then の中の関数を実行し、
-        return res.json() // 結果を json として読み込んで、次の then に渡す
-    })
-    .then((data) => {
-        // １つめが終わったら２つめの then の中の関数を実行します。
-        imageElement.src = data.message // 画像を表示する
-    })
+Button.textContent="更新"
+
+const image_update=function(){
+    // 指定したサーバーにデータを取りに行く
+    // fetch()を使うときは、ひとつめのthen()の中身は必ず同じで、ふたつめのthen()でdataを使ってなにか処理をする
+    fetch("https://dog.ceo/api/breeds/image/random")  // まず fetch でデータの取得をしにいき、
+        .then((res) => {
+            // fetch がおわったら１つめの then の中の関数を実行し、
+            return res.json() // 結果を json として読み込んで、次の then に渡す
+        })
+        .then((data) => {
+            // １つめが終わったら２つめの then の中の関数を実行します。
+            imageElement.src = data.message // 画像を表示する
+        })
+}
+
+Button.onclick=function(){
+    image_update()
+}
+
+image_update()
