a70ac06ed9d0c3faf1c5e7e9ee281fc0fc1a65e5
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-01-28 15:11:59 +0900

    □と〇に△を追加しました。

--- 04_ゼミ/week1/image_change/image.js
+++ 04_ゼミ/week1/image_change/image.js
@@ -1,11 +1,41 @@
 const figure = document.getElementById("figure")
+let count=0 
 
 figure.onclick = function() {
-    figure.classList.toggle("rounded")
+    if(count%3==0){
+        figure.classList.toggle("rounded")
+        count++
+    }
+    else if(count%3==1){
+        figure.classList.toggle("rounded")
+        figure.classList.toggle("triangle")
+        count++
+    }
+    else if(count%3==2){
+        figure.classList.toggle("triangle")
+        figure.classList.toggle("rounded")
+        figure.classList.toggle("rounded")
+        count++
+    }
+    console.log(count)
 }
 
 document.onkeydown=function(e){
     if(e.key=="Enter"){
-        figure.classList.toggle("rounded")
+        if(count%3==0){
+            figure.classList.toggle("rounded")
+            count++
+        }
+        else if(count%3==1){
+            figure.classList.toggle("rounded")
+            figure.classList.toggle("triangle")
+            count++
+        }
+        else if(count%3==2){
+            figure.classList.toggle("triangle")
+            figure.classList.toggle("rounded")
+            figure.classList.toggle("rounded")
+            count++
+        }
     }
 }
