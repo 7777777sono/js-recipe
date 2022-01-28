548fa071cbba1896128c3e066e2ca7b0be74c3fd
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-01-28 14:32:17 +0900

    □といろんなイベントをやりました。

--- 04_ゼミ/week1/image_change/image.js
+++ 04_ゼミ/week1/image_change/image.js
@@ -3,3 +3,9 @@ const figure = document.getElementById("figure")
 figure.onclick = function() {
     figure.classList.toggle("rounded")
 }
+
+document.onkeydown=function(e){
+    if(e.key=="Enter"){
+        figure.classList.toggle("rounded")
+    }
+}
