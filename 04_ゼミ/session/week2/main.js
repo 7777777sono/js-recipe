74c8446ce5fac3f3f8094ad3b1c348588eb3ae2a
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-01-28 12:54:41 +0900

    ペアワークで削除ボタンを作成しました。

--- 04_ゼミ/session/week2/main.js
+++ 04_ゼミ/session/week2/main.js
@@ -5,8 +5,16 @@ const cardsContainer=document.getElementById("memo-container")
 addButton.onclick=function(){
     console.log(MemoInput.value)
     const card = document.createElement("div") // <div></div>
+    const deleteButton= document.createElement("button") // <button></button>
     card.className = "card" // <div class="card></div>
     card.textContent = MemoInput.value // <div class="card>こんばんは</div>
+    deleteButton.id="delete-button"
+    deleteButton.textContent="削除"
     cardsContainer.append(card)
+    card.append(deleteButton)
+    deleteButton.onclick=function(){
+        card.remove()
+    }
     MemoInput.value=null
 }
+
