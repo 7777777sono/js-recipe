5a4fa7af064cc4a50457f038597c3e7b4f696818
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-02-05 20:40:08 +0900

    vue.jsの使い方の学習

--- 04_ゼミ/week1/prot1.js
+++ 04_ゼミ/week1/prot1.js
@@ -1,1 +1,26 @@
-console.log(100%(10**1))
+// const addAllNumbers = function(str){
+//     let num=0
+//     let strCount=0
+//     let char=""
+//     for(let i=0;i<str.length;i++){
+//         if(str[i]=="-"){
+//             for(let j=strCount;j<i;j++){
+//                 char=char+str[j]
+//                 console.log(char)
+//                 if(j==i-1){
+//                     num=num+Number(char)
+//                 }
+//                 char=""
+//             }
+//             strCount=i+1
+//         }
+//         else if(i==str.length-1&&str[i]!="-"){
+//             char=char+str[i]
+//             console.log(char)
+//             num=num+Number(char)
+//         }
+//     }
+//     console.log(num)
+// }
+
+// addAllNumbers("30-20-3")
---
+++ 04_ゼミ/week3/array/main.js
@@ -0,0 +1,8 @@
+const aisatu=["おはよう", "こんにちは", "こんばんは"]
+console.log(aisatu)
+aisatu.push("おやすみ")
+console.log(aisatu)
+aisatu.splice(1,1)
+console.log(aisatu)
+aisatu.splice(1,0,"Hello")
+console.log(aisatu)
