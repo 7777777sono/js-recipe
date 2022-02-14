6b941a993dea10c2c18424839d48716dc209bbe7
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-02-14 17:47:44 +0900

    おしゃれヒューマン

---
+++ 04_ゼミ/week5/stylish-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <Human v-for="cloth in cloths" v-bind:key="cloth" v-bind:cloth="cloth" v-bind:cloths="cloths"/>
+  </div>
+</template>
+
+<script>
+import Human from "@/components/Human.vue"
+
+export default {
+  components: { 
+    Human 
+  },
+  data(){
+    return {
+      cloths: [
+        "👚",
+        "👘",
+        "🧥",
+        "👕"
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+  .container {
+    display: flex;
+  }
+</style>
---
+++ 04_ゼミ/week5/stylish-app/src/components/Human.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="human">
+    <div class="atama">
+      <div>😊</div>
+    </div>
+    <div class="karada" v-on:click="change" v-if="judge">
+      💪{{cloth}}💪
+    </div>
+    <div class="karada" v-on:click="change" v-else>
+      💪{{currentCloth}}💪
+    </div>
+    <div class="ashi">
+      <div>🦵🦵</div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            cloth:{
+                type: String,
+                required: true
+            },
+            cloths:{
+                type: Array,
+                required: true
+            }
+        },
+        data() {
+            return {
+                number: 0,
+                currentCloth: ""
+            }
+        },
+        methods: {
+            change: function(){
+                this.number=Math.floor(Math.random() * this.cloths.length)
+                console.log(this.cloth===this.currentCloth)
+                this.currentCloth=this.cloths[this.number]
+            }
+        },
+        computed: {
+            judge: function(){
+                if(this.cloth===this.currentCloth||this.currentCloth===""){
+                    return true
+                }
+                else{
+                    return false
+                }
+            }
+        },
+        created() {
+            console.log(this.cloths)
+        }
+    }
+</script>
+
+<style>
+.human {
+  width: 100%;
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
