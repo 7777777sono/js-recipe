1a758c520fcfd4e276b7c6a19fa739fcf7c5487f
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-02-14 14:06:47 +0900

    コンポーネントの基本

---
+++ 04_ゼミ/week5/component-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <Human />
+    <Human />
+    <Human />
+    <Human />
+    <Human />
+  </div>
+</template>
+
+<script>
+import Human from "@/components/Human.vue";
+
+
+export default {
+    components: { 
+      Human, 
+    }
+}
+</script>
+
+<style>
+  .container {
+    display: flex;
+  }
+</style>
---
+++ 04_ゼミ/week5/component-app/src/components/Human.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="human">
+    <div class="atama">
+      <div v-if="isLaughing">🤣</div>
+      <div v-else>😊</div>
+    </div>
+    <div class="karada" v-on:click="makeLaugh">
+      <div>💪👕💪</div>
+    </div>
+    <div class="ashi">
+      <div>🦵🦵</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            isLaughing: false,
+        }
+    },
+    methods: {
+        makeLaugh: function(){
+            this.isLaughing=true
+        },
+    },
+}
+</script>
+
+<style>
+.human {
+  width: 100%;
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
