944b8899791497236e0176f4f58fcad3b278eeae
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-02-14 14:56:25 +0900

    コンポーネントの発展

---
+++ 04_ゼミ/week5/simplememo-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <simple-memo>
+    <button>メモ１ボタン</button>
+  </simple-memo>
+  <simple-memo>
+    <div>メモ２−１です</div>
+    <div>メモ２−２です</div>
+  </simple-memo> -->
+  <!-- <simple-memo bgColor="orange">オレンジ</simple-memo>
+  <simple-memo bgColor="pink">ピンク</simple-memo>
+  <simple-memo bgColor="green">みどり</simple-memo> -->
+  <!-- <simple-memo colorType="orange">オレンジ</simple-memo>
+  <simple-memo colorType="pink">ピンク</simple-memo>
+  <simple-memo colorType="green">みどり</simple-memo> -->
+  <simple-memo v-for="memo in memos" v-bind:key="memo.id" v-bind:memo="memo" />
+</template>
+
+<script>
+  import SimpleMemo from "@/components/SimpleMemo.vue"
+  export default {
+    components: { SimpleMemo },
+  data() {
+      return {
+        memos: [
+          { id: 0, text: "今日のメモ", createdAt: Date.now() },
+          { id: 1, text: "今週のメモ", createdAt: Date.now() - 60 * 60 * 24 * 3 },
+          { id: 2, text: "大昔のメモ", createdAt: Date.now() - 60 * 60 * 24 * 30 },
+        ],
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ 04_ゼミ/week5/simplememo-app/src/components/SimpleMemo.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="simple-memo" v-bind:class="memoClass">
+    {{ memo.text }}
+  </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            // colorType: {
+            //     validator(value) {
+            //         return ["orange", "pink", "green"].includes(value)
+            //     }
+            // },
+            memo: {
+                type: Object,
+                required: true
+            }
+        },
+        computed: {
+            memoClass() {
+                const date = this.memo.createdAt
+                const now = new Date()
+                const day = 60 * 60 * 24
+                const week = day * 7
+                if (now - date <= day) {
+                    return "memo__new"
+                } 
+                else if (now - date <= week) {
+                    return "memo__in-a-week"
+                } 
+                else {
+                    return "memo__old"
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.simple-memo {
+  margin: 4px;
+  padding: 0.5rem;
+  border-radius: 4px;
+}
+
+/* colorType */
+.orange {
+  background-color: orange;
+  color: white;
+}
+.pink {
+  background-color: pink;
+  color: red;
+}
+.green {
+  background-color: green;
+  color: white;
+}
+
+.memo__new {
+  background-color: orange;
+  color: white;
+}
+.memo__in-a-week {
+  background-color: pink;
+  color: white;
+}
+.memo__old {
+  background-color: snow;
+  color: gray;
+}
+</style>
