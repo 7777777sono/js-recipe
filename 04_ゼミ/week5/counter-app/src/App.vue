adc3368bfeadd828b327dcd31baea1a0e2d69781
7777777sono <xktqwp44d42krp6m9q5e@docomo.ne.jp>
2022-02-16 20:45:37 +0900

    初期値付きカウンター

---
+++ 04_ゼミ/week5/counter-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Counter v-bind:initialValue="122"/>
+</template>
+
+<script>
+import Counter from "@/components/Counter.vue";
+
+
+export default {
+    components: { 
+      Counter 
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 04_ゼミ/week5/counter-app/src/components/Counter.vue
@@ -0,0 +1,48 @@
+<template>
+    <div v-if="judge">{{count}}</div>
+    <div v-else>初期値が整数ではないので使用できません。</div>
+    <button v-on:click="countUp" v-bind:disabled="disabledJudge">+1</button>
+</template>
+
+<script>
+    export default{
+        props: {
+            initialValue: {
+                type: Number,
+                required: true
+            },
+        },
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            countUp: function(){
+                this.count++
+            }
+        },
+        computed: {
+            judge: function(){
+                if(Math.floor(this.count)===this.count){
+                    return true
+                }
+                else{
+                    return false
+                }
+            },
+            disabledJudge: function(){
+                if(Math.floor(this.count)===this.count){
+                    return false
+                }
+                else{
+                    return true
+                }
+            }
+        },
+        created() {
+            this.count=this.initialValue
+            console.log(this.count)
+        }
+    }
+</script>
